46e7c09e83efa822ca2a4430c4a2c2c038e920dc
zqk <1809950574@qq.com>
2025-08-18 16:02:52 +0800

    拆分了BrandCreateUseCase接口，创建了对应的BrandCreateService实现类服务

---
+++ mall-goods-application/src/main/java/com/mall/goods/application/port/in/BrandCreateUseCase.java
@@ -0,0 +1,13 @@
+package com.mall.goods.application.port.in;
+
+import com.mall.goods.application.command.CreateBrandCommand;
+
+public interface BrandCreateUseCase {
+    /**
+     * 创建品牌
+     *
+     * @param command 创建品牌命令
+     * @return 品牌ID
+     */
+    Long createBrand(CreateBrandCommand command);
+}
